325fc6fbecffd690c4bb95fdb367ca5ae6a45291
Ali Gilliland <11360403+algilliland@users.noreply.github.com>
2018-03-11 11:40:46 -0700

    Update grade.py
    Swapped in grade.py from PA1 since no command line arguments are needed

--- grade.py
+++ grade.py
@@ -1,50 +1,35 @@
-# grade.py
+# grade.py is a grading script that runs the given main class
+# on all of the inputs (*.in files) in the given input directory.
+# Does all this by copying the src/*.java files into TestingTemp/
+# and running the tests in TestingTemp/.
 #
-# usage:
-#   python grade.py MainClass TestDir [--outpre outfilePrefix] [--inext infileExt] [--gradescope]
-#
-# usage examples:
-#   python grade.py Section2Binary PublicTestCases --outpre binary 
-#
-# This grade.py is attempting to be more general for 
-# programs that accept command line input in addition
-# to or other than an input file name.  It assumes that
-# the output files in the given test directory end in .out.
-# It will then look for all outfiles with the given prefix.
-# After the prefix each command line option should be provided
-# in the outfile name separated by dashes.  The input file
-# if there is one will be provided without an extension.  The
-# optional argument to the grade script will be the infile extension
-# if the first command line option is a file.
-#
-# examples:
-#   binary-5.out
-#       The output prefix is "binary".  The program will be run with
-#       the command line "5".
-#
-#   python grade.py --mainclass PA2Main --testdir PublicTestCases --outpre pa2 --inext csv
-#   outfile: pa2-miniRoutes-MAX.out
-#       If I were going to redo the PA2 testing, this is what to do.
-#       In this one the output prefix is "pa2".
-#       Command line args: miniRoutes.csv MAX
-#
-
 # If the gradescope options is specified then the source
 # code will be found in "/autograder/submission/".
-# The script always generates a TestingTemp/results.json 
-# file that could be used by gradescope.
+# The script always generates a results.json file that could
+# be used by gradescope.
+#
+# usage examples:
+#   python grade.py PA1Main PublicTestCases --gradescope
+#   python grade.py PA1Main PublicTestCases
+#
+# The assumption is that each main is operating on a given file.
+# The input files in the specified input directory will be 
+# given to the program.  Here are the operations performed:
+#
+#   javac JavaHelloWorld.java
+#   java JavaHelloWorld PublicTestCases/file1.in
+#   java JavaHelloWorld PublicTestCases/file2.in
+#    ...
 #
 # Another assumption is that all of the source files are in the 
 # default package and are in the src/ subdirectory.
 # And the actual testing will happen in the testdir.
 # Also assuming the correctness grade is out of 50 points.
-import argparse
 import sys
 import os
 import json
 import subprocess
 import glob
-from functools import reduce
 
 #######################################
 # global variables
@@ -64,18 +49,22 @@ def getSubmissionSource(gradescope_flag):
 
 def cmdLineParse(argv):
 #######################################
-# Returns an args class with gradescope, mainclass, testdir, inext, and outpre specified.
-    parser = argparse.ArgumentParser(description="grading script for CS 210")
-    parser.add_argument('mainclass')
-    parser.add_argument('testdir')
-    parser.add_argument('--outpre', help='Prefix for expected outfiles.')
-    parser.add_argument('--inext', help='Extention for input files.')
-    parser.add_argument('--gradescope', help='Copy submission from gradescope location.',
-            action="store_true")
-    #DEBUG: parser.print_help()
-    args = parser.parse_args()
-    #DEBUG: print(args)
-    return args
+# usage examples:
+#   python grade.py PA1Main PublicTestCases --gradescope
+#   python grade.py PA1Main PublicTestCases
+# Returns (main class name, testcase directory, gradescope flag)
+    if len(sys.argv)<3:
+        print("usage:")
+        print("\tpython grade.py PA1Main PublicTestCases [--gradescope]")
+        sys.exit()
+    
+    # return the main class name, testcase directory, and
+    # whether gradescope should be run
+    gradescope_flag = 0
+    if len(sys.argv) >= 4:
+        gradescope_flag = sys.argv[3]=="--gradescope"
+
+    return (argv[1],argv[2],gradescope_flag)
 
 
 def copySrcToTempAndCDThere(srcdir, tempdir):
@@ -90,11 +79,8 @@ def copySrcToTempAndCDThere(srcdir, tempdir):
 
     # clean the director of previous files
     try:
-        print(tempdir+"*.java")
         os.remove(tempdir+"*.java")
-        os.remove(tempdir+"*.class")
     except OSError:
-        print("remove didn't work")
         pass
 
     # copy the source code over and move into directory
@@ -137,35 +123,36 @@ def formatFloat(inval):
 # Want floats in our score output to all have 2 decimal points
     return float("%.2f" % inval)
 
-def createTestRecord(mainclassname,expected_output_file,
-                     cmd_str,max_grade_per_test):
+def createTestRecord(mainclassname,infile,max_grade_per_test):
 #######################################
 # create a test record for each test
 # run the program and redirect to the "out" file
-# will compare with the given expected_output_file
-
-    # run the program
-    run_cmd = "java "+mainclassname+ " "+ cmd_str+" > out"
+    run_cmd = "java " + mainclassname + " " + infile + " > out"
     (run_retcode,run_output) = execCommand(run_cmd)
 
+    # truncate the floats for generated out and expected
+    basename = os.path.splitext(os.path.basename(infile))[0]
+    truncateFloats("out","trunc_out")
+    truncateFloats("../"+testdir+"/"+basename+".out", "trunc_expected")
+
     # do a diff with the generated output and the expected output
-    diff_cmd = "diff -B -w out "+expected_output_file
+    diff_cmd = "diff -B -w trunc_out trunc_expected"
     (diff_retcode,diff_output) = execCommand(diff_cmd)
         
     # put together all the information in the test record
     if diff_retcode!=0:
         score = 0.0
-        mesg = "Failed " + expected_output_file + " test.\n" \
-               + "*********** DIFF OUTPUT: Actual output followed by expected.\n"
-        print(mesg+diff_output)
+        mesg = "Failed " + infile + " test.\n" \
+               + "*********** OUTPUT: Actual output followed by expected.\n"
+        print(mesg)
     else:
         score = max_grade_per_test
-        mesg = "Passed " + expected_output_file + " test.\n"
+        mesg = "Passed " + infile + " test.\n"
         print(mesg)
 
     return { "score"       : formatFloat(score),
              "max_score"   : formatFloat(max_grade_per_test),
-             "name"        : expected_output_file,
+             "name"        : basename+'.in',
              "output"      : mesg + diff_output }
 
 
@@ -188,56 +175,22 @@ def compileProgram(mainclassname):
         return (True,mesg_prefix+' SUCCEEDED!\n')
 
 
-def parseOutFileName(outfile,outpre,infile_ext):
-#######################################
-# outfile is the outfile name in format discussed in above file header.
-# outpre is the outfile prefix.  Assuming not passed in if doesn't match.
-# infile_ext is None if there is no infile and a string if the first
-#   command line argument is an infile that needs an extension.
-#    
-# returns command str for use as command line arguments for program
-#
-# assumming names are in format outpre-infilebase-other-cmd-args.out
-# or outpre-cmd-args.out if there won't be an infile
-    #print("DEBUG: outfile=",outfile)
-    # take off the out extension
-    outfile = outfile[0:-4]
-
-    # outpre will now be followed by cmd line args
-    cmd_line_parts = outfile.split('-')
-    if (cmd_line_parts[0]!=outpre):
-        print("grade.py ERROR: file_name_parts[0]!=outpre")
-        sys.exit()
-    cmd_line_parts.pop(0)
-
-    # see if we need to grab an infile base
-    if (infile_ext!=None):
-        cmd_line_parts[0] = cmd_line_parts[0] + "." + infile_ext
-    # concat all the command line arguments with spaces between
-    cmd_str = reduce(lambda a,b: a+" "+b, cmd_line_parts, "")
-    #print("DEBUG: cmd_str=",cmd_str)
-   
-    return cmd_str
-
-
-def runTests(mainclassname,testdir,outpre,inext):
+def runTests(mainclassname,testdir):
 #######################################
 # returns (list of test records,total_score,failed_at_least_once flag)
 
-    # get a list of all the output files and max score per test
-    output_files = glob.glob("../"+testdir+"/"+outpre+"*.out")
-    max_grade_per_test = totalpoints/float(len(output_files))
+    # get a list of all the input files and max score per test
+    input_files = glob.glob("../"+testdir+"/*.in")
+    max_grade_per_test = totalpoints/float(len(input_files))
+    #print("DEBUG: max_grade_per_test = ", max_grade_per_test)
 
     # Do all of the tests
     test_records = []
     total_score = 0.0
     failed_at_least_once = False
-    for outfile in output_files:
-        cmd_str = parseOutFileName(os.path.basename(outfile),outpre,inext)
-        test_rect = createTestRecord(mainclassname, outfile, cmd_str,
-                                     max_grade_per_test)
-        failed_at_least_once = failed_at_least_once \
-                               or (test_rect["score"]==0)
+    for infile in input_files:
+        test_rect = createTestRecord(mainclassname,infile,max_grade_per_test)
+        failed_at_least_once = failed_at_least_once or (test_rect["score"]==0)
         total_score = total_score + test_rect["score"]
         test_records.append(test_rect)
 
@@ -249,23 +202,21 @@ def runTests(mainclassname,testdir,outpre,inext):
 # main python routine
 
 # set everything up and cd into temporary directory
-args = cmdLineParse(sys.argv)
-srcdir = getSubmissionSource(args.gradescope)
+(mainclassname, testdir, gradescope_flag) = cmdLineParse(sys.argv)
+srcdir = getSubmissionSource(gradescope_flag)
 copySrcToTempAndCDThere(srcdir, tempdir)
-
 # see https://gradescope-autograders.readthedocs.io/en/latest/specs/
 # for format of json file that will come from results_dict
 results_dict = {}
 
 #### try to compile the program
-(compile_succeeded,compile_msg) = compileProgram(args.mainclass)
+(compile_succeeded,compile_msg) = compileProgram(mainclassname)
 results_dict["output"] = compile_msg
 
 #### If compilation failed then done, else do testing
 if (compile_succeeded):
-    (test_records, total_score, failed_at_least_once) \
-            = runTests(args.mainclass,args.testdir,args.outpre,args.inext)
-    results_dict["score"] = round(formatFloat(total_score))
+    (test_records, total_score, failed_at_least_once) = runTests(mainclassname,testdir)
+    results_dict["score"] = formatFloat(total_score)
     results_dict["tests"] = test_records
 else:
     results_dict["score"] = 0.0
