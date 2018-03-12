4e017f22fefae0eab1fe7b620bf5dffa83ace32d
Ali Gilliland <11360403+algilliland@users.noreply.github.com>
2018-03-12 15:50:23 -0700

    Update Section7SudokuSearch.java
    Comments added

--- src/Section7SudokuSearch.java
+++ src/Section7SudokuSearch.java
@@ -24,20 +24,33 @@ public class Section7SudokuSearch {
             return true; // return true, puzzle solved 
         }
 
-        // Make local decision		
+        // Loop over possible local decisions 		
         // Avoid enumerating on values that do not work in the solution with noConflicts
-
         // While looping remember to remove last local decision 
+        
         return false; // all decisions tried, no solution found 
     }
 
-
+    /*
+     * This method should check that the num is a valid decision at the square located
+     * at the row, col in puzzle. 
+     * 
+     */
     private static boolean noConflicts(int row, int col, int [][] puzzle, int num) {
         // TODO Update to check the rows and columns in addition to the subgrid
         return !inSubGrid(puzzle, row - (row%3), col - (col%3), num);
     }
-
-    // Check if the number is in the subgrid already, true if it is, false if not
+    
+    /*
+     * This method is complete; look at this to understand how the puzzle's subgrids are
+     * being checked for valid choices.
+     * 
+     * puzzle - holds all decisions made so far and empty squares
+     * startRow and startCol - point to the upper corner of the subgrid to be checked
+     * num - Check if num is a valid decision in this subgrid
+     * 
+     * return true if num is already in the subgrid and false if the num is not 
+     */
     private static boolean inSubGrid(int [][] puzzle, int startRow, int startCol, int num) {
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
@@ -51,22 +64,25 @@ public class Section7SudokuSearch {
 
     private static boolean inColumn(int[][] puzzle, int col, int num) {
         // TODO Check the number is not already in the column of the unassigned square
+        // return false if it is not in the column and true if it is
         return false;
     }
 
     private static boolean inRow(int[][] puzzle, int row, int num) {
-        // TODO Check the number is not already in the row of the unassigned square
+        // TODO Check the number is not already in the row of the unassigned square 
+        // return false if it is not in the column and true if it is
         return false;
     }
 
     private static void process(int[][] puzzle) {
-        // TODO Print out the completed puzzle 
-
+        // TODO Print out the completed puzzle in a grid shape with no spaces
+        // Nested loops are acceptable here 
     }
 
     /*
      * TODO: Create a method that places the row and column of an empty 
-     * square into a DecisionPoint object. 
+     * square into a DecisionPoint object. Look at the DecisionPoint 
+     * class to see how to initialize them. 
      * 
      * Return first DecisionPoint object if there is an unassigned square 
      * and null if the suduko puzzle is full. 
