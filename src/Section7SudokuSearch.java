0ce1844d891136a35b31b4625943c093ec3e9c9c
Ali Gilliland <11360403+algilliland@users.noreply.github.com>
2018-03-12 13:07:00 -0700

    Update Section7SudokuSearch.java
    Spaces not tabs

--- src/Section7SudokuSearch.java
+++ src/Section7SudokuSearch.java
@@ -8,73 +8,70 @@
  * 
  */
 public class Section7SudokuSearch {
-	public static void main(String[] args) {
-		// A 9x9 array of ints represents the Sudoku puzzle 
-		enumerate(new int[9][9]);
+    public static void main(String[] args) {
+        // A 9x9 array of ints represents the Sudoku puzzle 
+        enumerate(new int[9][9]);
 
-	}
-	
-	public static boolean enumerate(int[][] puzzle) {
-		// Hold row col of unassigned square 
-		DecisionPoint nextMove = findUnassignedLoc(puzzle); 
-		
-		
-		if (nextMove == null) {
-			process(puzzle); 
-			return true; // return true, puzzle solved 
-		}
-		
-		// Make local decision
-		// Avoid enumerating on values that do not work in the solution with noConflicts
-		
-		// While looping remember to remove last local decision 
+    }
+    
+    static public boolean enumerate(int[][] puzzle) {
+        // Hold row col of unassigned square 
+        DecisionPoint nextMove = findUnassignedLoc(puzzle); 
 
-		return false; // all decisions tried, no solution found 
-	}
 
-	
-	private static boolean noConflicts(int row, int col, int [][] puzzle, int num) {
-		// TODO Update to check the rows and columns in addition to the subgrid
-		return !inSubGrid(puzzle, row - (row%3), col - (col%3), num);
-	}
+        if (nextMove == null) {
+            process(puzzle); 
+            return true; // return true, puzzle solved 
+        }
 
-	// Check if the number is in the subgrid already, true if it is, false if not
-	private static boolean inSubGrid(int [][] puzzle, int startRow, int startCol, int num) {
-		for (int row = 0; row < 3; row++) {
-			for (int col = 0; col < 3; col++) {
-				if (puzzle[row+startRow][col+startCol]  == num) {
-					return true; 
-				}
-			}
-		}
+        // Make local decision		
+        // Avoid enumerating on values that do not work in the solution with noConflicts
 
-		return false;
-	}
-	
-	private static boolean inColumn(int[][] puzzle, int col, int num) {
-		// TODO Check the number is not already in the column of the unassigned square
-		return false;
-	}
-	
-	private static boolean inRow(int[][] puzzle, int row, int num) {
-		// TODO Check the number is not already in the row of the unassigned square
-		return false;
-	}
-	
-	private static void process(int[][] puzzle) {
-		// TODO Print out the completed puzzle 
-		
-	}
-	
-	/*
-	 * TODO: Create a method that places the row and column of an empty 
-	 * square into the DecisionPoint object. 
-	 * 
-	 * Return a DecisionPoint object if there is an unassigned square and
-	 * null if the suduko puzzle is full. 
-	 */
-	private static DecisionPoint findUnassignedLoc(int[][] puzzle) {
-		return null;
-	}
+        // While looping remember to remove last local decision 
+        return false; // all decisions tried, no solution found 
+    }
+
+
+    private static boolean noConflicts(int row, int col, int [][] puzzle, int num) {
+        // TODO Update to check the rows and columns in addition to the subgrid
+        return !inSubGrid(puzzle, row - (row%3), col - (col%3), num);
+    }
+
+    // Check if the number is in the subgrid already, true if it is, false if not
+    private static boolean inSubGrid(int [][] puzzle, int startRow, int startCol, int num) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (puzzle[row+startRow][col+startCol]  == num) {
+                    return true; 
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean inColumn(int[][] puzzle, int col, int num) {
+        // TODO Check the number is not already in the column of the unassigned square
+        return false;
+    }
+
+    private static boolean inRow(int[][] puzzle, int row, int num) {
+        // TODO Check the number is not already in the row of the unassigned square
+        return false;
+    }
+
+    private static void process(int[][] puzzle) {
+        // TODO Print out the completed puzzle 
+
+    }
+
+    /*
+     * TODO: Create a method that places the row and column of an empty 
+     * square into the array rc. 
+     * 
+     * Return true if there is an unassigned square and false if the 
+     * suduko puzzle is full. 
+     */
+    private static DecisionPoint findUnassignedLoc(int[][] puzzle) {
+        return null;	
+    }
 }
-
