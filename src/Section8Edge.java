bc157edaa5b2d53a225de0f4df0b88d0065c8c6a
Ali Gilliland <11360403+algilliland@users.noreply.github.com>
2018-03-18 21:40:24 -0700

    Update Section8Edge.java
    Start code in place

--- src/Section8Edge.java
+++ src/Section8Edge.java
@@ -8,70 +8,123 @@
  * 
  */
 public class Section8Edge implements Comparable<Section8Edge> {
+
     private int node1;
+
     private int node2;
 
+
+
+
     public Section8Edge(int n1, int n2) {
+
         node1 = n1;
+
         node2 = n2;
-    }
-    
-    /*
-     * (non-Javadoc)
-     * @see java.lang.Object#toString()
-     * 
-     * 
-     */
-    public String toString() {
-        return "    " + node1 + " -- " + node2 + ";";
-    }
-    
-    /*
-     * (non-Javadoc)
-     * @see java.lang.Object#equals(java.lang.Object)
-     * 
-     * Checks if two edges are equal and returns true if they are.  
-     * If the two edges are not equal return false. 
-     *  
-     *  ex) 1 -- 2 == 2 -- 1 is true 
-     *      1 -- 3 == 1 -- 3 is true 
-     *      1 -- 3 == 1 -- 3 is false  
-     */
-    @Override 
-    public boolean equals(Object e) {
-        if (this.node1 == ((Section8Edge) e).node1
-                && this.node2 == ((Section8Edge) e).node2) {
-            return true; 
-        }
-        if (this.node2 == ((Section8Edge) e).node1
-                && this.node1 == ((Section8Edge) e).node2) {
-            return true; 
-        } 
-        return false; 
-    }
-    
-    /*
-     * (non-Javadoc)
-     * @see java.lang.Object#hashCode()
-     * 
-     * We overrode equals so we need to over ride hashCode. 
-     */
-    public int hashCode() {
-        return (node1 + node2);
+
     }
 
-    @Override
-    public int compareTo(Section8Edge other) {
-        if (this.equals(other)) {
-            return 0; // this and other are equal 
-        }
-        else if ((node1 < other.node1)
-                || (node1 == other.node1 && node2 < other.node2)) {
-            return -1; // this is less than other
-        }
-        else {
-            return 1; // this is greater than other
-        }
-    }
     
+
+    /*
+
+     * (non-Javadoc)
+
+     * @see java.lang.Object#toString()
+
+     * 
+
+     * TODO: Match the string output for edges shown in 
+
+     * the file usage comment in Section8Main and the writeup. 
+
+     */
+
+    public String toString() {
+
+        return "";
+
+    }
+
+    
+
+    
+
+    /*
+
+     * (non-Javadoc)
+
+     * @see java.lang.Object#equals(java.lang.Object)
+
+     * 
+
+     *  TODO: complete this method so that two edges that connect the same two nodes
+
+     *  are equal - return true. If the two edges are not equal return false. 
+
+     *  
+
+     *  ex) 1 -- 2 == 2 -- 1 is true 
+
+     *      1 -- 3 == 1 -- 3 is true 
+
+     *      1 -- 3 == 1 -- 3 is false  
+
+     */
+
+    @Override 
+
+    public boolean equals(Object e) {
+
+        return false; 
+
+    }
+
+    
+
+    /*
+
+     * (non-Javadoc)
+
+     * @see java.lang.Object#hashCode()
+
+     * 
+
+     * We overrode equals so we need to over ride hashCode. 
+
+     */
+
+    public int hashCode() {
+
+        return (node1 + node2);
+
+    }
+
+
+
+
+    /*
+
+     * (non-Javadoc)
+
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+
+     * 
+
+     *  TODO: complete this method so that two edges can be compared 
+
+     *  for the TreeSet to maintain order. 
+
+     */
+
+    @Override
+
+    public int compareTo(Section8Edge o) {
+
+        return 0; 
+
+    }
+
+    
+
 }
